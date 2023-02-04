ae3a6bfba00bec7faa6635f600c92a60d310e5cb
azwarbahar07 <azwarbahar07@gmail.com>
2023-02-04 13:22:47 +0800

    kgfdjlkgdf

---
+++ api/updatePetaniOneData.php
@@ -0,0 +1,25 @@
+<?php
+require_once '../koneksi.php';
+header('Content-type: application/json');
+error_reporting(E_ERROR | E_PARSE);
+
+$id = $_POST['id'];
+$key = $_POST['key'];
+$value = $_POST['value'];
+
+$query = "UPDATE tb_petani SET $key = '$value',
+                                updated_at = NULL WHERE id = '$id'";
+
+if (mysqli_query($conn, $query)) {
+
+    $result["kode"] = "1";
+    $result["pesan"] = "Success!";
+
+    echo json_encode($result);
+    mysqli_close($conn);
+} else {
+    $response["kode"] = "0";
+    $response["pesan"] = "Error! " . mysqli_error($conn);
+    echo json_encode($response);
+    mysqli_close($conn);
+}
