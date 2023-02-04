c076a75ae67834c2c7ba6b959531b7e10ebed60b
azwarbahar07 <azwarbahar07@gmail.com>
2023-02-04 12:57:52 +0800

    api photo

---
+++ api/updatePhotoPetani.php
@@ -0,0 +1,32 @@
+<?php
+require_once '../koneksi.php';
+header('Content-type: application/json');
+error_reporting(E_ERROR | E_PARSE);
+
+$id = $_POST['id'];
+
+// SET FOTO
+$foto = $_FILES['foto']['name'];
+$ext = pathinfo($foto, PATHINFO_EXTENSION);
+$nama_foto = "image_" . time() . "." . $ext;
+$file_tmp = $_FILES['foto']['tmp_name'];
+
+$query = "UPDATE tb_petani SET foto = '$nama_foto',
+                                updated_at = NULL WHERE id = '$id'";
+
+
+if (mysqli_query($conn, $query)) {
+
+    move_uploaded_file($file_tmp, '../assets/images/photo/' . $nama_foto);
+
+    $result["kode"] = "1";
+    $result["pesan"] = "Success!";
+
+    echo json_encode($result);
+    mysqli_close($conn);
+} else {
+    $response["kode"] = "0";
+    $response["pesan"] = "Error! " . mysqli_error($conn);
+    echo json_encode($response);
+    mysqli_close($conn);
+}
