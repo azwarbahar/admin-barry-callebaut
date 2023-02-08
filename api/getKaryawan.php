6746ba019db24d6d7449c7b0e4ece531a8107d39
azwarbahar07 <azwarbahar07@gmail.com>
2023-02-08 20:01:41 +0800

    hghgytttt

---
+++ api/getKaryawan.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../koneksi.php';
+header('Content-type: application/json');
+// error_reporting(E_ERROR | E_PARSE);
+
+$query = "SELECT * FROM tb_karyawan";
+$result = mysqli_query($conn, $query);
+
+$array = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $array[] = $row;
+}
+
+echo ($result) ?
+    json_encode(array("kode" => "1", "karyawan_data" => $array)) :
+    json_encode(array("kode" => "0", "pesan" => "Data tidak ditemukan"));
