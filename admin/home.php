2d27ce69364ffd73ffe738c0b0f21d0a6767be60
Fadli <jaya.fadly@gmail.com>
2023-03-01 15:31:15 +0700

    ubah nama selaat datang

--- admin/home.php
+++ admin/home.php
@@ -15,7 +15,7 @@ require_once '../template/header.php';
             <div class="row">
                 <div class="col-sm-12">
                     <h4 class="page-title">Dashboard</h4>
-                    <p class="text-muted page-title-alt">Selamat datang di admin Barry Callebaut !</p>
+                    <p class="text-muted page-title-alt">Selamat datang di admin Ishaq !</p>
                 </div>
             </div>
 
