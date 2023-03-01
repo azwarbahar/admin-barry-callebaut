e1588bd15da99b8d076adcd3b7d89131de00d11c
Fadli <jaya.fadly@gmail.com>
2023-03-01 20:01:59 +0700

    md 8

--- admin/home.php
+++ admin/home.php
@@ -187,7 +187,7 @@ require_once '../template/header.php';
                     </div>
                 </div>
                 
-                 <div class="col-lg-4" style="overflow-y: scroll; height: 300px; width: 100%;">
+                 <div class="col-8" style="overflow-y: scroll; height: 300px; width: 100%;">
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="portlet">
