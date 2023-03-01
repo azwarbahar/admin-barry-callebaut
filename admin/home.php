958ec2040ca6e156db02dc4cf3eab90b97c6ec84
Fadli <jaya.fadly@gmail.com>
2023-03-01 20:06:23 +0700

    lg 444

--- admin/home.php
+++ admin/home.php
@@ -187,7 +187,7 @@ require_once '../template/header.php';
                     </div>
                 </div>
                 register_shutdown_function
-                 <div class="col-lg-3" style="overflow-y: scroll; height: 300px; width: 100%;">
+                 <div class="col-lg-4" style="overflow-y: scroll; height: 300px; width: 100%;">
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="portlet">
