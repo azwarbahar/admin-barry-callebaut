95ea68d58ba83390163d89bd562273f9d48f459f
Fadli <jaya.fadly@gmail.com>
2023-03-05 12:42:08 +0700

    dark

--- admin/home.php
+++ admin/home.php
@@ -75,7 +75,7 @@ require_once '../template/header.php';
 
                     <div class="portlet" style= "background: green">
                         <div class="portlet-heading portlet-default">
-                            <h3 class="portlet-title text-dark" style="color:black">
+                            <h3 class="portlet-title" style="color:green">
                                 Aktivitas field facilitator
                             </h3>
                             <div class="portlet-widgets">
