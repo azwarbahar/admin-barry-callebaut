565be19283050e351f94c496260b84e63d5597f5
Fadli <jaya.fadly@gmail.com>
2023-03-05 13:15:02 +0700

    ghgh

--- admin/home.php
+++ admin/home.php
@@ -70,9 +70,10 @@ require_once '../template/header.php';
                 </div>
             </div>
 
-            <div class="col-lg-8">
-                        		<div class="card-box">
-                                    <a href="#" class="pull-right btn btn-default btn-sm waves-effect waves-light">View All</a>
+            <div class="row">
+                <div class="col-lg-8" style="overflow-y: scroll; height:300px; weight: 1400px">
+
+                    <div class="portlet" style= "background: green">
                         <div class="portlet-heading portlet-default">
                             <h3 class="portlet-title text-dark">
                                 Aktivitas field facilitator
