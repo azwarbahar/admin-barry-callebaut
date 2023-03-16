a379f4707e366df2a1cd2946c51b3920feb18853
Fadli <jaya.fadly@gmail.com>
2023-03-16 19:27:48 +0700

    atyyyyyyt

--- admin/home.php
+++ admin/home.php
@@ -71,10 +71,10 @@ require_once '../template/header.php';
             </div>
 
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                     <div class="card-box">
                         <a href="#" class="pull-right btn btn-default btn-sm waves-effect waves-light">View All</a>
-                        <h4 class="text-dark header-title m-t-0">Recent Activities</h4>
+                        <h4 class="text-dark header-title m-t-0">Aktifitas Petugas Lapangan</h4>
                         <p class="text-muted m-b-30 font-13">
 							Your awesome text goes here. Yeah !!
 						</p>
