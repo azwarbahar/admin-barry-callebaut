2047be43a308dd3201c786ba36623a5322087d18
Fadli <jaya.fadly@gmail.com>
2023-06-16 16:47:05 +0700

    70

--- admin/home.php
+++ admin/home.php
@@ -182,7 +182,7 @@ require_once '../template/header.php';
                                 </div>
                                 <div class="clearfix"></div>
                             </div>
-                            <div class="nicescroll" style="height: 266px;">
+                            <div class="nicescroll" style="height: 270px;">
                             <div id="bg-danger" class="panel-collapse collapse in">
                                 <div class="portlet-body">
                                     
