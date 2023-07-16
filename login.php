ef5be945996aab66e73926e6b22aec6527cea59d
Fadli <jaya.fadly@gmail.com>
2023-07-17 00:49:47 +0700

    apk

--- login.php
+++ login.php
@@ -61,7 +61,7 @@ if (isset($_POST['login'])) {
 
     <link rel="shortcut icon" href="../assets/images/favicon_1.ico">
 
-    <title>Barry Callebaut - Admin Dashboard</title>
+    <title>Aplikasi Ishaq - Admin Dashboard</title>
 
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
